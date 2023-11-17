27e11950888fdddb500e856f8a5c0ee022a4d0a6
SoLowMow <110025570+solowmow@users.noreply.github.com>
2023-11-17 02:32:11 +0100

    Update

---
+++ OpenCalc.kt
@@ -0,0 +1,13 @@
+// OpenCalc.kt - Kotlin Example (Simplified)
+
+object OpenCalc {
+    fun add(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun subtract(a: Int, b: Int): Int {
+        return a - b
+    }
+
+    // Implement other mathematical operations as needed
+}
