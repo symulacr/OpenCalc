ca397a462c8ddd8cf7bb004487d4361895620558
SoLowMow <110025570+solowmow@users.noreply.github.com>
2023-11-17 02:30:52 +0100

    Update

---
+++ OpenCalc.rb
@@ -0,0 +1,13 @@
+# OpenCalc.rb - Ruby Example (Simplified)
+
+class OpenCalc
+    def self.add(a, b)
+        a + b
+    end
+
+    def self.subtract(a, b)
+        a - b
+    end
+
+    # Implement other mathematical operations as needed
+end
