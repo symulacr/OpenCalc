bd0a2b02f62b480c2ae5166b28709a3f7e32927e
SoLowMow <110025570+solowmow@users.noreply.github.com>
2023-11-17 02:27:09 +0100

    Update

---
+++ OpenCalcKEVM.sol
@@ -0,0 +1,13 @@
+// OpenCalcKEVM.sol - KEVM Solidity-like Smart Contract (Example)
+
+contract OpenCalcKEVM {
+    function add(uint256 a, uint256 b) public pure returns (uint256) {
+        return a + b;
+    }
+
+    function subtract(uint256 a, uint256 b) public pure returns (uint256) {
+        return a - b;
+    }
+
+    // Implement other mathematical operations as needed
+}
