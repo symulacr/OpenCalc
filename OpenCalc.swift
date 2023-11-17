a7c1f0a3caf4780474956535d3d2a6e05b59fbaf
SoLowMow <110025570+solowmow@users.noreply.github.com>
2023-11-17 02:29:22 +0100

    Update

---
+++ OpenCalc.swift
@@ -0,0 +1,12 @@
+// OpenCalc.swift - Swift Example (Simplified)
+
+// Implementing basic mathematical functions
+func add(a: Int, b: Int) -> Int {
+    return a + b
+}
+
+func subtract(a: Int, b: Int) -> Int {
+    return a - b
+}
+
+// Implement other mathematical operations as needed
